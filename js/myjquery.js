b98cdbf5d7eb8f97a241467e2b0774178a486605
Hammad-10 <idreeshammad579@gmail.com>
2024-08-05 12:54:39 +0500

    Your commit message

--- js/myjquery.js
+++ js/myjquery.js
@@ -9,13 +9,13 @@ $("#brandStoryPara").animate({right: '274px'}, 2000);
 
 $("#organicSelectionHeading ").animate({left: '274px'}, 2000);
 
+
+
 $(".organicProducts").mouseenter(function(){
     $(".productTitle").fadeIn(2000);
 $(".productDescription").fadeIn(2000);
 
 })
-
-
 
 
 
